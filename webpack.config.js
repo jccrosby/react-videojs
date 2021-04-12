8595e575a39dd347819fb31e4f89e7b5541bb12c
John Crosby <john.crosby@mlb.com>
2021-04-12 12:54:29 -0400

    fixed webpack and added build dir to ignore

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'build'),
     },
-    resolve = {
+    resolve: {
         extensions: ['.ts', '.tsx', '.js', '.jsx'],
     },
     module: {
